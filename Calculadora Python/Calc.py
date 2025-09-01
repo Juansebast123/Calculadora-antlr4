d64a0e6265347e1fe186b6fa361345a0556057f3
Juansebast123 <166045022+Juansebast123@users.noreply.github.com>
2025-09-01 16:59:02 -0500

    Add files via upload

---
+++ Calculadora Python/Calc.py
@@ -0,0 +1,18 @@
+import sys
+from antlr4 import *
+from LabeledExprLexer import LabeledExprLexer
+from LabeledExprParser import LabeledExprParser
+from EvalVisitor import EvalVisitor
+
+def main():
+    input_stream = InputStream(sys.stdin.read())
+    lexer = LabeledExprLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = LabeledExprParser(stream)
+    tree = parser.prog()
+
+    eval = EvalVisitor()
+    eval.visit(tree)
+
+if __name__ == '__main__':
+    main()
